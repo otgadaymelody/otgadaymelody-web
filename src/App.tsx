308cf5d1d446efa068a460d66fb78f4478a6f80a
Anton Basov <bvs0tkin@MacBook-Air-Anton.local>
2023-01-23 01:09:10 +0600

    Create: Simple route | Base folders structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import './App.css';
+import './styles/App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Otgaday Melody Website</h1>
-    </div>
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<Home />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
   );
 }
 
