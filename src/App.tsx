d55d90876cb37b875f5989c40fe46ae79d9a8785
Anton Basov <bvs0tkin@MacBook-Air-Anton.local>
2023-01-23 02:06:21 +0600

    fix(app): bug in imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './assets/css/App.js';
+import './assets/css/App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 
