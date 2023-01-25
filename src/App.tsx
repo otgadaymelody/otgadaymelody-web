41a589a2d50d5bc451b5777bd0b69d7e34b041cc
Boris Ermachenko <ermachenkoboris@gmail.com>
2023-01-25 23:03:11 +0300

    refactor(project): add gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Home from './pages/Home';
 function App() {
 	return (
 		<div className="App">
-			<BrowserRouter>
+			<BrowserRouter basename={process.env.PUBLIC_URL}>
 				<Routes>
 					<Route path='/' element={<Home />} />
 				</Routes>
