58ceafd31a31944255db93eb850e69f279471e4e
Anton Basov <bvs0tkin@MacBook-Air-Anton.local>
2023-01-25 13:31:54 +0600

    refactor(project): remove fonts initializaton from index.css and create testing global style variables

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 
+
+import './index.css';
+import './assets/css/main.css';
+import './assets/css/fonts.css'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
