a529dce64df6188dda1037a12d8545ddddbecc3c
Anton Basov <bvs0tkin@MacBook-Air-Anton.local>
2023-02-02 22:08:11 +0600

    refactor(block-background): change adding external styles on util function

--- src/components/block-background/BlockBackground.tsx
+++ src/components/block-background/BlockBackground.tsx
@@ -5,15 +5,16 @@ import { ReactComponent as TopLeftMediator } from '../../assets/images/home-page
 import { ReactComponent as TopRightMediator } from '../../assets/images/home-page/mediator-top-right.svg'
 import { ReactComponent as BottomLeftMediator } from '../../assets/images/home-page/mediator-bottom-left.svg'
 import { ReactComponent as BottomRightMediator } from '../../assets/images/home-page/mediator-bottom-right.svg'
+import { concatStyles } from '../../utils/concat-styles'
 
 const BlockBackground: FC<BlockBackgroundProps> = ({mediatorsClasses,...props}) => {
   return (
     <div className={['block-background', props.className].join(' ')}>
       {props.children}
-      <TopLeftMediator className={['block-background__mediator', mediatorsClasses.topLeft].join(' ')}/>
-      <TopRightMediator className={['block-background__mediator', mediatorsClasses.topRight].join(' ')}/>
-      <BottomLeftMediator className={['block-background__mediator', mediatorsClasses.bottomLeft].join(' ')}/>
-      <BottomRightMediator className={['block-background__mediator', mediatorsClasses.bottomRight].join(' ')}/>
+      <TopLeftMediator className={concatStyles('block-background__mediator', mediatorsClasses.topLeft)}/>
+      <TopRightMediator className={concatStyles('block-background__mediator', mediatorsClasses.topRight)}/>
+      <BottomLeftMediator className={concatStyles('block-background__mediator', mediatorsClasses.bottomLeft)}/>
+      <BottomRightMediator className={concatStyles('block-background__mediator', mediatorsClasses.bottomRight)}/>
    </div>
   )
 }
