140dd33596228a60e9ac11ea72a01856c002f629
Anton Basov <bvs0tkin@MacBook-Air-Anton.local>
2023-02-02 18:26:48 +0600

    feat(base-button): add posibility to connect external styles

--- src/components/base-button/BaseButton.tsx
+++ src/components/base-button/BaseButton.tsx
@@ -1,16 +1,28 @@
 import React, { FC } from 'react'
+import { concatStyles } from '../../utils/concat-styles';
 import './BaseButton.css'
 
 interface BaseButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
    title?: string;
    img?: string;
+   styles?: {
+      buttonForm: string,
+      buttonTitle: string
+   }
 }
 
-const BaseButton: FC<BaseButtonProps> = ({title, img, ...props}) => {
+const BaseButton: FC<BaseButtonProps> = ({title, img, styles, ...props}) => {
   return (
-      <button className={["base-btn", props.className].join(' ')} onClick={() => console.log("Click BTN")}>
+      <button 
+         className={styles?.buttonForm ? concatStyles("base-btn", styles.buttonForm) : "base-btn"} 
+         onClick={() => console.log("Click BTN")}
+      >
          {img && <img className='base-btn__image' src={img} alt=""/>}
-         {title && <span className='base-btn__title'>{title}</span>}
+         {title && 
+            <span 
+               className={styles?.buttonTitle ? concatStyles("base-btn__title", styles.buttonTitle) : "base-btn__title"} >{title}
+            </span>
+         }
       </button>
   )
 }
