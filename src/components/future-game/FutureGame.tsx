a9d5ef6fd868cbcfc1307d1976289838f7500415
Anton Basov <bvs0tkin@MacBook-Air-Anton.local>
2023-02-13 12:21:50 +0600

    feat(future-game): add location and price images and styles for they

--- src/components/future-game/FutureGame.tsx
+++ src/components/future-game/FutureGame.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import GameRegistrationButton from '../ui/game-reg-button/GameRegistrationButton';
 import './FutureGame.css';
+import { ReactComponent as LocationImg } from '../../assets/images/future-game/Location.svg';
+import { ReactComponent as PriceImg } from '../../assets/images/future-game/Ticket.svg';
 
 const FutureGame = (): React.ReactElement => {
   return (
@@ -12,8 +14,14 @@ const FutureGame = (): React.ReactElement => {
       <div className="future-game__description-block">
         <div className="future-game__description">
           <div className="future-game__main-description">
-            <div className="future-game__place">Place</div>
-            <div className="future-game__price">$$$</div>
+            <div className="future-game__place">
+              <LocationImg />
+              <span>Price</span>
+            </div>
+            <div className="future-game__price">
+              <PriceImg />
+              <span>$$$</span>
+            </div>
           </div>
           <div className="future-game__address">Address</div>
         </div>
