c95e1247d2cb1ef7047157089a6cb39410d1d63b
Anton Basov <bvs0tkin@MacBook-Air-Anton.local>
2023-02-15 13:11:42 +0600

    feat(faq-block): add toggle effect

--- src/components/faq-block/FAQBlock.tsx
+++ src/components/faq-block/FAQBlock.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BlockBackground from '../block-background/BlockBackground';
+import { faqQuestions } from './faqQuestions';
+import openImg from '../../assets/images/home-page/open-img.svg';
+import closeImg from '../../assets/images/home-page/close-img.svg';
 import './FAQBlock.css';
 
 const FAQBlock = (): React.ReactElement => {
+  const [selected, setSelected] = useState<number | null>(null);
   const mediatorClasses = {
     topLeft: 'banner__mediator_top-left',
     topRight: 'banner__mediator_top-right',
@@ -10,13 +14,35 @@ const FAQBlock = (): React.ReactElement => {
     bottomRight: 'banner__mediator_bottom-right',
   };
 
+  const toggle = (i: number): void => {
+    if (selected === i) {
+      return setSelected(null);
+    }
+
+    setSelected(i);
+  };
+
   return (
     <BlockBackground className="faq-block" mediatorsClasses={mediatorClasses}>
       <div className="faq-block__title">Остались вопросы?</div>
       <ul className="faq-block__questions-list">
-        <li>
+        {faqQuestions.map((item, i) => (
+          <div className="faq-block__question" key={i}>
+            <div className="faq-block__question-title" onClick={() => toggle(i)}>
+              {item.question}
+              <img src={selected === i ? closeImg : openImg} />
+            </div>
+            <div className={selected === i ? 'faq-block__answer show' : 'faq-block__answer'}>
+              {item.answer}
+            </div>
+          </div>
+        ))}
+        {/* <li>
           <span className="faq-block__question-title">Вопрос 1.</span>
-          <div className="faq-block__open-action"></div>
+          <div
+            className={openState ? 'faq-block__close-action' : 'faq-block__open-action'}
+            onClick={() => setStateAction(!openState)}
+          ></div>
         </li>
         <li>
           <span className="faq-block__question-title">Вопрос 2.</span>
@@ -25,7 +51,7 @@ const FAQBlock = (): React.ReactElement => {
         <li>
           <span className="faq-block__question-title">Вопрос 3.</span>
           <div className="faq-block__open-action"></div>
-        </li>
+        </li> */}
       </ul>
     </BlockBackground>
   );
---
+++ src/components/faq-block/faqQuestions.ts
@@ -0,0 +1,14 @@
+export const faqQuestions = [
+  {
+    question: 'Вопрос 1',
+    answer: 'Ответ 1',
+  },
+  {
+    question: 'Вопрос 2',
+    answer: 'Ответ 2',
+  },
+  {
+    question: 'Вопрос 3',
+    answer: 'Ответ 3',
+  },
+];
