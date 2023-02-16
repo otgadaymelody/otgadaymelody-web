fd8fad34c5ef3ab40160fc118a5c7e9835c478f9
Anton Basov <bvs0tkin@MacBook-Air-Anton.local>
2023-02-16 12:21:00 +0600

    feat(about-us): add styles for text

--- src/components/about-us/AboutUs.tsx
+++ src/components/about-us/AboutUs.tsx
@@ -4,19 +4,26 @@ import BaseButton from '../ui/base-button/BaseButton';
 import './AboutUs.css';
 
 const AboutUs = (): React.ReactElement => {
+  const regBtnStyles = {
+    buttonForm: 'game-rules__reg-btn',
+    buttonTitle: 'game-rules__reg-btn-title',
+  };
+
   return (
     <div className="about-us">
       <div className="game-rules">
         <h1 className="game-rules__title">Как проходят игры?</h1>
         <img src={aboutUsPreview} className="game-rules__preview-img" />
-        <span>
+        <span className="game-rules__description">
           Командная игра победить в которой помогут эрудиция логика и сообразительность. отгадай
           мелодию - хороший способ командная игра победить в которой помогут эрудиция логика и
           сообразительность
         </span>
-        <h2>Нет команды?</h2>
-        <span>Не проблема - регистрируйся на игру, а мы найдем команду!</span>
-        <BaseButton title="Регистрация на игру" />
+        <h2 className="game-rules__subtitle">Нет команды?</h2>
+        <span className="game-rules__description">
+          Не проблема - регистрируйся на игру, а мы найдем команду!
+        </span>
+        <BaseButton title="Регистрация на игру" styles={regBtnStyles} />
       </div>
 
       <div className="infographic-block">Блок с инфографикой</div>
