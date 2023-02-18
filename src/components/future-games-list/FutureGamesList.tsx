63f0df6d441b4754768b58d0af244cbe949a07a3
Anton Basov <bvs0tkin@MacBook-Air-Anton.local>
2023-02-18 14:40:30 +0600

    feat(future-games-list): add slider

--- src/components/future-games-list/FutureGamesList.tsx
+++ src/components/future-games-list/FutureGamesList.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import BlockBackground from '../block-background/BlockBackground';
 import FutureGame from './future-game/FutureGame';
 import BaseButton from '../ui/base-button/BaseButton';
 import { gameList } from './gameList';
 import './FutureGamesList.css';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import { Navigation } from 'swiper';
+import useDeviceType from '../../hooks/useDeviceType';
+
 const FutureGamesList = (): React.ReactElement => {
+  const deviceType = useDeviceType();
+  const isDesktop = deviceType === 'desktop';
   const mediatorClasses = {
     topLeft: 'future-games-list__mediator_top-left',
     topRight: 'future-games-list__mediator_top-right',
@@ -21,11 +30,35 @@ const FutureGamesList = (): React.ReactElement => {
   return (
     <BlockBackground className="future-games-list" mediatorsClasses={mediatorClasses}>
       <h2 className="future-games-list__title">Предстоящие игры</h2>
-      <div className="future-games-list__games_list">
-        <FutureGame className={'future-games-list__game'} game={gameList[0]} />
-        <FutureGame className={'future-games-list__game'} game={gameList[0]} />
-        <FutureGame className={'future-games-list__game'} game={gameList[0]} />
-      </div>
+      {isDesktop ? (
+        <Swiper
+          navigation={true}
+          slidesPerView={3}
+          spaceBetween={32}
+          modules={[Navigation]}
+          className="future-games-list__games_list"
+        >
+          <SwiperSlide>
+            <FutureGame className={'future-games-list__game'} game={gameList[0]} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <FutureGame className={'future-games-list__game'} game={gameList[0]} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <FutureGame className={'future-games-list__game'} game={gameList[0]} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <FutureGame className={'future-games-list__game'} game={gameList[0]} />
+          </SwiperSlide>
+        </Swiper>
+      ) : (
+        <div className="future-games-list__games_list">
+          <FutureGame className={'future-games-list__game'} game={gameList[0]} />
+          <FutureGame className={'future-games-list__game'} game={gameList[0]} />
+          <FutureGame className={'future-games-list__game'} game={gameList[0]} />
+        </div>
+      )}
+
       {gameList.length > 3 && <BaseButton styles={showMoreBtnClasses} title="Показать еще" />}
     </BlockBackground>
   );
