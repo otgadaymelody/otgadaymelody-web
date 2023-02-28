60abbac0813d6467d7b9e8fc537de73127bd1f68
Anton Basov <bvs0tkin@MacBook-Air-Anton.local>
2023-02-28 22:44:29 +0600

    feat(interesting-facts): add styles

--- src/components/interesting-facts/interesting-fact-slide/InterestingFactSlide.tsx
+++ src/components/interesting-facts/interesting-fact-slide/InterestingFactSlide.tsx
@@ -8,8 +8,8 @@ const InterestingFactSlide = (): React.ReactElement => {
       <div className="interesting-fact-slide__title">Интересный факт</div>
       <FactImgBlock />
       <div className="interesting-fact-slide__fact-block">
-        Музыка действительно влияет на состояние нашего организма <br />
-        <br /> Ваше сердце подстраивается под ритм музыки, которую вы слушаете.
+        <p>Музыка действительно влияет на состояние нашего организма </p>
+        <p>Ваше сердце подстраивается под ритм музыки, которую вы слушаете.</p>
       </div>
     </div>
   );
