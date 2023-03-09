493ceec068224cda2ed4d726d31590531fe83301
Anton Basov <bvs0tkin@MacBook-Air-Anton.local>
2023-03-09 11:57:14 +0600

    fix(interesting-fact-slide): change divs to semantic tags

--- src/components/interesting-facts/interesting-fact-slide/InterestingFactSlide.tsx
+++ src/components/interesting-facts/interesting-fact-slide/InterestingFactSlide.tsx
@@ -4,14 +4,14 @@ import './InterestingFactSlide.css';
 
 const InterestingFactSlide = (): React.ReactElement => {
   return (
-    <div className="interesting-fact-slide">
-      <div className="interesting-fact-slide__title">Интересный факт</div>
+    <article className="interesting-fact-slide">
+      <h1 className="interesting-fact-slide__title">Интересный факт</h1>
       <FactImgBlock />
-      <div className="interesting-fact-slide__fact-block">
+      <section className="interesting-fact-slide__fact-block">
         <p>Музыка действительно влияет на состояние нашего организма </p>
         <p>Ваше сердце подстраивается под ритм музыки, которую вы слушаете.</p>
-      </div>
-    </div>
+      </section>
+    </article>
   );
 };
 
