d096b83cd4ee2d58cf50e285eefd7dd4b5571390
Anton Basov <bvs0tkin@MacBook-Air-Anton.local>
2023-03-14 13:50:01 +0600

    refactor(interesting-facts): remove svg images and add new facts and images for it

---
+++ src/components/interesting-facts/facts.consts.ts
@@ -0,0 +1,68 @@
+import fact01 from '../../assets/images/home-page/interesting-facts/01.png';
+import fact02 from '../../assets/images/home-page/interesting-facts/02.png';
+import fact03 from '../../assets/images/home-page/interesting-facts/03.png';
+import fact04 from '../../assets/images/home-page/interesting-facts/04.png';
+import fact05 from '../../assets/images/home-page/interesting-facts/05.png';
+import fact06 from '../../assets/images/home-page/interesting-facts/06.png';
+import fact07 from '../../assets/images/home-page/interesting-facts/07.png';
+import fact08 from '../../assets/images/home-page/interesting-facts/08.png';
+import fact09 from '../../assets/images/home-page/interesting-facts/09.png';
+import fact10 from '../../assets/images/home-page/interesting-facts/10.png';
+
+export declare interface factType {
+  image: string;
+  description: string;
+}
+
+export const FACTS_LIST: factType[] = [
+  {
+    image: fact01,
+    description:
+      'В 2009 году Britney Spears выбрала поп-рок группу «Ранетки» для выступления на разогреве своего шоу «The Circus Starring» в Московских «Лужниках».',
+  },
+  {
+    image: fact02,
+    description:
+      'В 1999 году Минская группа «Леприконсы» выпустила композицию под названием «Хали-гали, паратрупер», где в тексте припева перечисляются названия аттракционов в культовом, столичном парке Челюскинцев.',
+  },
+  {
+    image: fact03,
+    description:
+      'Название группы «Комбинация», по одной версии, происходит от комбинации музыкальных стилей, которые исполняла группа. По другой — отсылает к виду женского белья.',
+  },
+  {
+    image: fact04,
+    description:
+      'В честь немецкой всемирно известной рок группы «Rammstein» назван астероид в центральной области пояса астероидов. Астероид открыт 11 октября 2001 года французским астрономом Жаном-Клодом Мерленом в Обсерватории Ле-Кресо.',
+  },
+  {
+    image: fact05,
+    description:
+      'В 1996 году на концерте рок-группы «Агата Кристи», фронтмен, Вадим Самойлов разыграл свой личный автомобиль среди поклонников пришедших на концерт.',
+  },
+  {
+    image: fact06,
+    description:
+      'Вокальная партия музыкальной композиции «На Заре», группы Альянс, является одной их самых сложных в истории музыки. Диапазон вокальной мелодии более 2,5 октав, что больше любого вокального произведения (1,5 октавы) и любого оперного произведения (2 октавы).',
+  },
+  {
+    image: fact07,
+    description:
+      'В 2016 году Леонид Агутин был номинирован на престижную премию «Грэмми» за записанный альбом на испанском языке «La Vida Cosmopolita».',
+  },
+  {
+    image: fact08,
+    description:
+      'Псевдоним французского исполнителя Stromae, на «Верлане» произносится как maesto. «Верлан» — тайный французский язык, в котором слоги или звуки идут задом наперед.',
+  },
+  {
+    image: fact09,
+    description:
+      'Юрий Хой, фронтмен, музыкальной панк-рок группы «Сектор Газа» успел поработать инспектором в ГАИ до того как стал известен.',
+  },
+  {
+    image: fact10,
+    description:
+      'Отечественный поп-исполнитель Мурат Насыров исполнил музыкальные темы к заставкам мультсериалов «Черный плащ» и «Утиные истории», которые впоследствии обрели культовый статус.',
+  },
+];
