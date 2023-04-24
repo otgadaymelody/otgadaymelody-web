42deff0357a4ea2e77f161e44a0e38306412ed72
bvs0tkin <bvs0tkin@gmail.com>
2023-04-25 00:11:38 +0600

    fix(interesting-facts): change aspect-ratio for facts images

--- src/components/interesting-facts/interesting-fact-slide/InterestingFactSlide.tsx
+++ src/components/interesting-facts/interesting-fact-slide/InterestingFactSlide.tsx
@@ -5,7 +5,7 @@ import { type InterestingFactSlideProps } from './InterestingFactSlideProps';
 const InterestingFactSlide: FC<InterestingFactSlideProps> = ({ fact }): React.ReactElement => {
   return (
     <article className="interesting-fact-slide">
-      <h1 className="interesting-fact-slide__title">Интересный факт</h1>
+      <h2 className="interesting-fact-slide__title">Интересный факт</h2>
       <img src={fact.image} className="interesting-fact-slide__image" />
       <section className="interesting-fact-slide__description">{fact.description}</section>
     </article>
