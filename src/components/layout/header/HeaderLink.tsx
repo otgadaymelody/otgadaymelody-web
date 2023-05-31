986d73f70da4063f068788f02a33181942c5c5e3
bvs0tkin <bvs0tkin@gmail.com>
2023-05-31 09:58:17 +0600

    fix(burger-menu): change icons and colors, add close menu for clicking on link

--- src/components/layout/header/HeaderLink.tsx
+++ src/components/layout/header/HeaderLink.tsx
@@ -1,20 +1,27 @@
 import React, { type FC } from 'react';
 import { Link } from 'react-router-dom';
+import { concatStyles } from '../../../utils/concat-styles';
 
 interface HeaderLinkProps {
   title: string;
   img: string[];
   url: string;
   hash?: string;
+  linkTitleColor?: string;
+  onClick?: React.MouseEventHandler;
 }
 
-const HeaderLink: FC<HeaderLinkProps> = ({ title, img, url, hash }) => {
+const HeaderLink: FC<HeaderLinkProps> = ({ title, img, url, hash, linkTitleColor, onClick }) => {
+  let titleStyles = concatStyles('header__header-link__title', 'header-link__title');
+  if (linkTitleColor === 'white') {
+    titleStyles = concatStyles(titleStyles, 'header-link__white');
+  }
   const to = hash ? { pathname: `/${url}`, hash: `${hash}` } : `/${url}`;
   return (
-    <Link className="header__header-link header-link" to={to}>
+    <Link className="header__header-link header-link" to={to} onClick={onClick}>
       <img className="header-link_hide-on-click" src={img[0]} alt="img"></img>
       <img className="header-link_show-on-click" src={img[1]} alt="img"></img>
-      <span className="header__header-link__title header-link__title">{title}</span>
+      <span className={titleStyles}>{title}</span>
     </Link>
   );
 };
