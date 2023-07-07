fa6e59d56469054452f122f4d30d89f6ae485ffa
alsu <Felis_07@inbox.ru>
2023-07-07 15:55:34 +0200

    fix(games-page):move icons

--- src/components/games/games-map/games-map-address/games-map.consts.ts
+++ src/components/games/games-map/games-map-address/games-map.consts.ts
@@ -3,6 +3,6 @@ export const GAMES_MAP_LIST = [
     day: { header: '6', text: 'янв' },
     time: { header: '19:00', text: 'Пятница' },
     price: { header: '500 р', text: 'С человека' },
-    location: { header: '⚐ Пивзавод', text: 'Зеленский Съезд, 10' },
+    location: { header: '📍 Пивзавод', text: 'Зеленский Съезд, 10' },
   },
 ];
