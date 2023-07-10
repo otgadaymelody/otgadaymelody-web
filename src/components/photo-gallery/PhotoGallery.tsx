4b6aaea3b11cdd4d1b1385c4f5c6c25b154a42c4
KirillovaD <sdaria@inbox.ru>
2023-07-10 22:01:28 +0300

    fix(swiper): edit swiper method

--- src/components/photo-gallery/PhotoGallery.tsx
+++ src/components/photo-gallery/PhotoGallery.tsx
@@ -1,9 +1,9 @@
-import React, {type FC} from 'react';
+import React, { type FC } from 'react';
 import './PhotoGallery.css';
 import { type BaseComponent } from '../../shared/interfaces/baseComponent';
 import { PHOTO_GALLERY_LIST } from './photoGalleryList.consts';
-import { Swiper,SwiperSlide} from 'swiper/react';
-import {EffectCoverflow, Navigation} from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectCoverflow, Navigation } from 'swiper';
 
 import sliderNextImg from '../../assets/images/future-game/slider-next.svg';
 import sliderPrevImg from '../../assets/images/future-game/slider-prev.svg';
@@ -12,18 +12,17 @@ import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/navigation';
 
-
-const PhotoGallery: FC<BaseComponent> = ({className}): React.ReactElement => {
+const PhotoGallery: FC<BaseComponent> = ({ className }): React.ReactElement => {
   const [loopedSlides, setLoopedSlides] = React.useState<number>(1);
   const navigateToVkPost = (): void => {
     window.location.href = 'https://vk.com/wall-164712588_7382';
   };
- 
-  const onLoopedSlidesChange = (swiper: any):void => {
-      setLoopedSlides(2);
-      swiper.params.loopedSlides = loopedSlides
-  }
-  
+
+  const onLoopedSlidesChange = (swiper: any): void => {
+    setLoopedSlides(2);
+    swiper.params.loopedSlides = loopedSlides;
+  };
+
   return (
     <div className={`photo-gallery ${className}`}>
       <h1 className="photo-gallery__title">Посмотрите сами у&nbsp;нас всегда круто!</h1>
@@ -49,19 +48,26 @@ const PhotoGallery: FC<BaseComponent> = ({className}): React.ReactElement => {
             prevEl: '.photo-gallery__slider-button-prev',
           }}
           loopedSlides={loopedSlides}
-          onSlideChange={(swiper) => {onLoopedSlidesChange(swiper)}}
+          onBeforeSlideChangeStart={(swiper) => {
+            onLoopedSlidesChange(swiper);
+          }}
         >
           {PHOTO_GALLERY_LIST.map((item, key) => (
             <SwiperSlide key={key}>
-              <img className="photo-gallery__photo" src={item} onClick={navigateToVkPost} alt={`Фото ${key}`}/>
+              <img
+                className="photo-gallery__photo"
+                src={item}
+                onClick={navigateToVkPost}
+                alt={`Фото ${key}`}
+              />
             </SwiperSlide>
           ))}
-          <div className='photo-gallery__buttons-wrapper'>
+          <div className="photo-gallery__buttons-wrapper">
             <div className="photo-gallery__slider-button-prev">
-              <img src={sliderNextImg} alt={'Кнопка назад'}/>
+              <img src={sliderNextImg} alt={'Кнопка назад'} />
             </div>
             <div className="photo-gallery__slider-button-next">
-              <img src={sliderPrevImg} alt={'Кнопка вперед'}/>
+              <img src={sliderPrevImg} alt={'Кнопка вперед'} />
             </div>
           </div>
         </Swiper>
