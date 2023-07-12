e0ebbfbd73d4a089b2c02df879dc3bfd401b4613
KirillovaD <sdaria@inbox.ru>
2023-07-12 19:10:48 +0300

    fix(main-banner):change photo, add styles

--- src/components/main-banner/MainBanner.tsx
+++ src/components/main-banner/MainBanner.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import BaseButton from '../ui/base-button/BaseButton';
 import BlockBackground from '../block-background/BlockBackground';
 import './MainBanner.css';
-import bannerImg from '../../assets/images/home-page/banner-image.png';
+import bannerImg from '../../assets/images/home-page/main-banner-photo.png';
+import dots from '../../assets/images/home-page/dots.png';
 import useDeviceType from '../../hooks/useDeviceType';
 
 const MainBanner = (): React.ReactElement => {
@@ -13,12 +14,12 @@ const MainBanner = (): React.ReactElement => {
   const deviceType = useDeviceType();
   const isMobile = deviceType === 'mobile';
 
-  const mediatorClasses = {
-    topLeft: 'banner__mediator_top-left',
-    topRight: 'banner__mediator_top-right',
-    bottomLeft: 'banner__mediator_bottom-left',
-    bottomRight: 'banner__mediator_bottom-right',
-  };
+  // const mediatorClasses = {
+  //   topLeft: 'banner__mediator_top-left',
+  //   topRight: 'banner__mediator_top-right',
+  //   bottomLeft: 'banner__mediator_bottom-left',
+  //   bottomRight: 'banner__mediator_bottom-right',
+  // };
 
   const registrationBtnClasses = {
     buttonForm: 'banner__reg-btn',
@@ -26,7 +27,7 @@ const MainBanner = (): React.ReactElement => {
   };
 
   return (
-    <BlockBackground className="banner__background" mediatorsClasses={mediatorClasses}>
+    <div className="banner__background">
       <div className="banner__info">
         <div className="banner__main-text">
           <h1>
@@ -34,19 +35,11 @@ const MainBanner = (): React.ReactElement => {
             <span className="banner__info_bold-title">КВИЗ ИГРЫ</span>
           </h1>
           <article className="banner__description-wrapper">
-            <p className="banner__description">
-              Надоели однотипные мероприятия или хочется попробовать чего-нибудь новенького?
-            </p>
             {!isMobile && (
               <p className="banner__description">
-                Тогда добавьте в свою жизнь красок и драйва вместе с «Отгадай Мелодию». Уникальный
-                формат мероприятия, песни, которые Вы точно знаете и любите, жаркие танцы,
-                интерактивное караоке, розыгрыши и огромное количество подарков!
-              </p>
-            )}
-            {!isMobile && (
-              <p className="banner__description">
-                Уверены, что это будет любовь с первого взгляда.
+                Добавьте красок и драйва в жизнь с &quot;Отгадай Мелодию&ldquo;. Уникальный формат
+                мероприятия с популярными песнями, танцами, караоке, розыгрышами и множеством
+                подарков! Любовь с первого взгляда гарантирована.
               </p>
             )}
           </article>
@@ -59,11 +52,14 @@ const MainBanner = (): React.ReactElement => {
         />
       </div>
       <div className="banner-image">
-        <div className="banner-image__wrapper">
-          <img src={bannerImg} className="banner-image__img" />
-        </div>
+        <img src={bannerImg} className="banner-image__img" />
       </div>
-    </BlockBackground>
+      {isMobile && (
+        <div className="banner__dots">
+          <img src={dots} className="banner__dots__img" />
+        </div>
+      )}
+    </div>
   );
 };
 
