13b968664902040a1203c4d4a9c5a69713f91ef2
alsu <Felis_07@inbox.ru>
2023-07-14 18:10:00 +0200

    feat(games-pages):added component imagevector and fix layout

--- src/components/games/games-main.tsx
+++ src/components/games/games-main.tsx
@@ -5,20 +5,32 @@ import GamesBanner from './games-banner/games-banner';
 import Description from './description/description';
 import GamesMap from './games-map/games-map';
 import FAQBlock from '../faq-block/FAQBlock';
-
+import ImageVector from './image-vector/ImageVector';
+import firstImageUrl from '../../assets/images/image-vector/gamesmain__imagevector-block-img-first.png';
+import secondImageUrl from '../../assets/images/image-vector/Vector 37.png';
 const GamesMain: FC = () => {
   return (
     <section className="gamesmain__container">
-      <div className="gamesmain_registartion-container">
-        <GamesBanner></GamesBanner>
-        <RegistrationForm></RegistrationForm>
+      <div className="gamesmain__mapregistartion-container">
+        <div className="gamesmain__registartion-container">
+          <GamesBanner></GamesBanner>
+          <RegistrationForm></RegistrationForm>
+        </div>
+        <div className="gamesmain__map-container">
+          <GamesMap></GamesMap>
+          <Description></Description>
+        </div>
       </div>
-      <div className="gamesmain_map-container">
-        <GamesMap></GamesMap>
-        <Description></Description>
-      </div>
-      <div className="gamesmain_block-container">
-        <FAQBlock className="gamesmain_block-part"></FAQBlock>
+      <div className="gamesmain__block-container">
+        <ImageVector
+          game="game1"
+          className="gamesmain__block-imagepart"
+          firstImageUrl={firstImageUrl}
+          secondImageUrl={secondImageUrl}
+          firstImageClassName="gamesmain__block-imagepart-image"
+          secondImageClassName="gamesmain__block-imagepart-vector"
+        ></ImageVector>
+        <FAQBlock className="gamesmain__block-faqpart"></FAQBlock>
       </div>
     </section>
   );
---
+++ src/components/games/image-vector/ImageVector.tsx
@@ -0,0 +1,30 @@
+import React, { type FC } from 'react';
+import './ImageVector.css';
+
+interface ImageVectorProps {
+  game: string;
+  className: string;
+  firstImageUrl: string;
+  secondImageUrl: string;
+  firstImageClassName?: string;
+  secondImageClassName?: string;
+}
+
+const ImageVector: FC<ImageVectorProps> = ({
+  game,
+  className,
+  firstImageUrl,
+  secondImageUrl,
+  firstImageClassName,
+  secondImageClassName,
+  ...props
+}) => {
+  return (
+    <div className={`image-vector ${className}`} {...props}>
+      <img src={firstImageUrl} alt={`${game} First`} className={firstImageClassName} />
+      <img src={secondImageUrl} alt={`${game} Second`} className={secondImageClassName} />
+    </div>
+  );
+};
+
+export default ImageVector;
