678fd550f5b7f87c0e9060f9ed79ba34d3252dbf
Elena Musina <info@speakfrench.ru>
2023-07-15 20:36:33 +0300

    feat(ui): create error notification component

--- src/pages/franchise.tsx
+++ src/pages/franchise.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import BestOfBlock from '../components/best-of/best-of-block';
 import FranchiseAboutUs from '../components/franchise-about-us/franchise-about-us';
-import Download from '@components/franchise-download/download';
-import WhyUs from '@components/franchise-why-us/franchise-why-us';
-import FranAudience from '@components/franchise-audience/franchise-audience';
+import Download from '../components/franchise-download/download';
+import WhyUs from '../components/franchise-why-us/franchise-why-us';
+import FranAudience from '../components/franchise-audience/franchise-audience';
 
 const Franchise = (): React.ReactElement => {
   return (
