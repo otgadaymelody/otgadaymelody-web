bd4858a98c990665b65e7bf9d64294e0c79b5928
Elena Musina <info@speakfrench.ru>
2023-07-18 16:42:49 +0300

    fix(ui): small notification component fixes

--- src/components/ui/notifications/notification-error.tsx
+++ src/components/ui/notifications/notification-error.tsx
@@ -1,5 +1,7 @@
 import React, { type FC } from 'react';
 import './notification-error.css';
+import ErrorImage from './notif-images/notif-error-img.svg';
+import CloseImage from './notif-images/notif-close-img.svg';
 
 interface NotificationProps {
   message: string;
@@ -8,32 +10,13 @@ interface NotificationProps {
 const NotificationError: FC<NotificationProps> = ({ message }) => {
   return (
     <article className="notification-error__container">
-      <div className="notification-error__message_wrapper">
-        <svg
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <rect width="24" height="24" rx="4" fill="#FF4056" />
-          <path d="M17.5 6L6.5 17" stroke="white" strokeWidth="2" strokeLinecap="round" />
-          <path d="M6.5 6L17.5 17" stroke="white" strokeWidth="2" strokeLinecap="round" />
-        </svg>
+      <div className="notification-error__message-wrapper">
+        <img src={ErrorImage}></img>
         <p className="notification-error__message">{message}</p>
       </div>
 
       <button className="notification-error__close-btn">
-        <svg
-          width="16"
-          height="16"
-          viewBox="0 0 16 16"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M13.5 2.5L2.5 13.5" stroke="#111827" strokeWidth="1.5" strokeLinecap="round" />
-          <path d="M2.5 2.5L13.5 13.5" stroke="#111827" strokeWidth="1.5" strokeLinecap="round" />
-        </svg>
+        <img src={CloseImage}></img>
       </button>
     </article>
   );
--- src/components/ui/notifications/notification-success.tsx
+++ src/components/ui/notifications/notification-success.tsx
@@ -1,5 +1,7 @@
 import React, { type FC } from 'react';
 import './notification-success.css';
+import SuccessImage from './notif-images/notif-success-img.svg';
+import CloseImage from './notif-images/notif-close-img.svg';
 
 interface NotificationProps {
   message: string;
@@ -8,37 +10,13 @@ interface NotificationProps {
 const NotificationSuccess: FC<NotificationProps> = ({ message }) => {
   return (
     <article className="notification-success__container">
-      <div className="notification-success__message_wrapper">
-        <svg
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <rect width="24" height="24" rx="4" fill="#C2D72F" />
-          <path
-            d="M17.5 7.5L11 15.5L6.5 11.5"
-            stroke="white"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
+      <div className="notification-success__message-wrapper">
+        <img src={SuccessImage}></img>
         <p className="notification-success__message">{message}</p>
       </div>
 
       <button className="notification-success__close-btn">
-        <svg
-          width="16"
-          height="16"
-          viewBox="0 0 16 16"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M13.5 2.5L2.5 13.5" stroke="#111827" strokeWidth="1.5" strokeLinecap="round" />
-          <path d="M2.5 2.5L13.5 13.5" stroke="#111827" strokeWidth="1.5" strokeLinecap="round" />
-        </svg>
+        <img src={CloseImage}></img>
       </button>
     </article>
   );
