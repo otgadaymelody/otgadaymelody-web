d00671f841031ad65f0d9be7daa8a8d072db321c
Ilvina <ilvrakh@gmail.com>
2023-07-20 18:33:49 +0500

    feat(footer): added media styles and desktop restyle

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,12 +1,8 @@
 import React, { type FC } from 'react';
 import './Footer.css';
 import { ReactComponent as Logo } from '@assets/images/footer/logo-white.svg';
-// import { ReactComponent as WaveImg } from '@assets/images/footer/wave.svg';
 import useDeviceType from '../../../hooks/useDeviceType';
-// import womenImage from '@assets/images/footer/women.png';
 import footerIcon from '@assets/images/footer/footer-icon.svg';
-// import logo from '@assets/images/footer/logo-white.svg';
-
 import { type BaseComponent } from '../../../shared/interfaces/baseComponent';
 import { SOCIAL_URL } from '../../../shared/consts/socialURLs.consts';
 
@@ -17,29 +13,19 @@ const Footer: FC<BaseComponent> = ({ className }): React.ReactElement => {
 
   return (
     <footer className={`footer ${className}`}>
-      <div className="container footer__container">
-        {/* {isDesktop && (
-          <div className="footer__composition">
-            <img src={womenImage} className="footer__women" />
-          </div>
-        )} */}
-        {(isDesktop || isTabletLg) && <img src={footerIcon} className="footer__icon" />}
-        <div className="wrapper">
-          <Logo className="footer-logo" />
-          <h1 className="footer__title">Следите за нами</h1>
-          <div className="footer__social-btns">
-            <a
-              className="footer__vk-btn"
-              href={SOCIAL_URL.VK}
-              target="_blank"
-              rel="noopener noreferrer"
-            />
-          </div>
+      {(isDesktop || isTabletLg) && <img src={footerIcon} className="footer__icon" />}
+      <div className="footer__infoblock">
+        <Logo className="footer-logo" />
+        <h1 className="footer__title">Следите за нами</h1>
+        <div className="footer__social-btns">
+          <a
+            className="footer__vk-btn"
+            href={SOCIAL_URL.VK}
+            target="_blank"
+            rel="noopener noreferrer"
+          />
         </div>
       </div>
-      {/* <div className="footer__waves">
-        <WaveImg />
-      </div> */}
     </footer>
   );
 };
