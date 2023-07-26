4479582f475b1c16d9b699333e31ba5e2f6cb100
Alena Nemtsova <jelenatbs@gmail.com>
2023-07-26 15:03:02 +0300

    fix(acc-franchisee): delete unused

--- src/components/acc-franchisee/franchisee-games/FranchiseeGames.tsx
+++ src/components/acc-franchisee/franchisee-games/FranchiseeGames.tsx
@@ -1,19 +1,16 @@
 import React, { type FC } from 'react';
-import './FranchiseeGames.css';
+import GamesSwitch from './games-switch/GamesSwitch';
 import BaseButton from '../../ui/base-button/BaseButton';
 import FranciseeGamesTable from './franchisee-games-table/FranchiseeGamesTable';
+import './FranchiseeGames.css';
 
 const FranciseeGames: FC = () => {
   return (
     <section className="franchisee-games">
-      <div className="franchisee-games__bar bar">
-        <div className="bar__wrapper">
-          <h2 className="franchisee-games__title">Игры</h2>
-          <div className="games-toggle-btn">
-            <input type="radio" className="toggle-switch" />
-          </div>
-          <BaseButton title="Добавить игру" />
-        </div>
+      <div className="franchisee-games__bar">
+        <h2 className="franchisee-games__title">Игры</h2>
+        <GamesSwitch />
+        <BaseButton title="Добавить игру" />
       </div>
       <FranciseeGamesTable />
     </section>
