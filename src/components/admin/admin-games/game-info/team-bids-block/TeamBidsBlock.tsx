cbd242e78450b24aeb34ddf88927676d1cca20e1
Alena Nemtsova <jelenatbs@gmail.com>
2023-08-21 03:34:36 +0300

    fix(admin-games): add useEffect dependency array

--- src/components/admin/admin-games/game-info/team-bids-block/TeamBidsBlock.tsx
+++ src/components/admin/admin-games/game-info/team-bids-block/TeamBidsBlock.tsx
@@ -1,50 +1,47 @@
-import React, { useEffect, useState } from 'react'
-import './TeamBidsBlock.css'
-import SortDataBlock from './sort-data-block/SortDataBlock'
-import TeamBidData from './team-bid-data/TeamBidData'
+import React, { useEffect, useState } from 'react';
+import './TeamBidsBlock.css';
+import SortDataBlock from './sort-data-block/SortDataBlock';
+import TeamBidData from './team-bid-data/TeamBidData';
 import { TeamBidsData } from './team-bid-data/TeamBidsData.consts';
 
+const TeamBidsBlock: React.FC = (): React.ReactElement => {
+  const [filtersData, setFiltersData] = useState(TeamBidsData);
+  const [propToFilter, setPropToFilter] = useState('all');
 
-
-const TeamBidsBlock: React.FC  = (): React.ReactElement => {
-  const [filtersData, setFiltersData] = useState(TeamBidsData)
-  const [propToFilter, setPropToFilter] = useState('all')
-  
   const handleSort = (propToFilter: string): void => {
-    setPropToFilter(propToFilter)
-    if(propToFilter === 'all') {
-      setFiltersData(TeamBidsData.filter(a => a.status !== 'deleted'))
+    setPropToFilter(propToFilter);
+    if (propToFilter === 'all') {
+      setFiltersData(TeamBidsData.filter((a) => a.status !== 'deleted'));
     } else {
-      setFiltersData(TeamBidsData.filter(a => a.status === propToFilter))
+      setFiltersData(TeamBidsData.filter((a) => a.status === propToFilter));
     }
-  } 
+  };
   useEffect(() => {
-    handleSort(propToFilter)
-  })
-  
-  
-  return (
-    <div className='game-info__applications'>
-        <h3 className="game-info__title">Заявки на игру</h3>
-        <div className='game-info__block game-applications_wrapper'>
-            <SortDataBlock onChange={handleSort}/>
-            <div className='game-applications_scrollBlock'>
-                {filtersData.map((item, index) => (
-                <TeamBidData
-                key={index}
-                applicationId={item.applicationId}
-                gameId={item.gameId}
-                playersCount={item.playersCount}
-                contactPlayerName={item.contactPlayerName}
-                contactPlayerPhone={item.contactPlayerPhone}
-                status={item.status}
-                teamName={item.teamName}
-                />
-                ))}
-            </div>  
-        </div>
-    </div>
-  )
-}
+    handleSort(propToFilter);
+  }, []);
 
-export default TeamBidsBlock
+  return (
+    <div className="game-info__applications">
+      <h3 className="game-info__title">Заявки на игру</h3>
+      <div className="game-info__block game-applications_wrapper">
+        <SortDataBlock onChange={handleSort} />
+        <div className="game-applications_scrollBlock">
+          {filtersData.map((item, index) => (
+            <TeamBidData
+              key={index}
+              applicationId={item.applicationId}
+              gameId={item.gameId}
+              playersCount={item.playersCount}
+              contactPlayerName={item.contactPlayerName}
+              contactPlayerPhone={item.contactPlayerPhone}
+              status={item.status}
+              teamName={item.teamName}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TeamBidsBlock;
