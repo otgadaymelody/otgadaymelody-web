fbd945fdc6f44930ff382f71bdb2d9022765daca
admin <admin@MacBook-Pro-admin.local>
2023-09-11 06:42:48 +0300

    feat(project): remove redirect

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,11 @@ import './assets/css/layout.css';
 import Layout from '@components/layout/Layout';
 
 function App(): React.ReactElement {
-  document.location.href = 'https://teletype.in/@otgaday.melody';
-  return <div className="App">{/* <Layout /> */}</div>;
+  return (
+    <div className="App">
+      <Layout />
+    </div>
+  );
 }
 
 export default App;
